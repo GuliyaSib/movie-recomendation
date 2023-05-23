6207aa9e7e79cb181dd0b3dae13f3f5abcd97113
GuliyaSib <sib.guliya@gmail.com>
2023-05-23 15:52:36 +0300

    test keras

---
+++ model.py
@@ -0,0 +1,86 @@
+import pandas as pd
+import numpy as np
+from datetime import date
+# To create deep learning models
+from keras.layers import Input, Embedding, Reshape, Dot, Concatenate, Dense, Dropout
+from keras.models import Model
+from sklearn.metrics import mean_squared_error
+
+
+#movie_metadata = pd.read_csv('movies_metadata.csv', low_memory=False)[['id','original_title', 'overview', 'vote_count']].dropna()
+#movie_metadata.set_index('id', inplace=True)
+
+df_filterd = pd.read_csv('data.csv')
+
+# Testingsize
+n = 10000
+
+# Split train- & testset
+df_train = df_filterd[:-n]
+df_test = df_filterd[-n:]
+train_user_data = df_train['userId']
+train_movie_data = df_train['movieId']
+test_user_data = df_test['userId']
+test_movie_data = df_test['movieId']
+
+user_id_mapping = {id:i for i, id in enumerate(df_filterd['userId'].unique())}
+movie_id_mapping = {id:i for i, id in enumerate(df_filterd['movieId'].unique())}
+
+# Get input variable-sizes
+users = len(user_id_mapping)
+movies = len(movie_id_mapping)
+embedding_size = 10
+
+# Setup variables
+user_embedding_size = 20
+movie_embedding_size = 10
+
+
+##### Create model
+# Set input layers
+user_id_input = Input(shape=[1], name='user')
+movie_id_input = Input(shape=[1], name='movie')
+
+# Create embedding layers for users and movies
+user_embedding = Embedding(output_dim=user_embedding_size, 
+                           input_dim=users,
+                           input_length=1, 
+                           name='user_embedding')(user_id_input)
+movie_embedding = Embedding(output_dim=movie_embedding_size, 
+                            input_dim=movies,
+                            input_length=1, 
+                            name='item_embedding')(movie_id_input)
+
+# Reshape the embedding layers
+user_vector = Reshape([user_embedding_size])(user_embedding)
+movie_vector = Reshape([movie_embedding_size])(movie_embedding)
+
+# Concatenate the reshaped embedding layers
+concat = Concatenate()([user_vector, movie_vector])
+
+# Combine with dense layers
+dense = Dense(256)(concat)
+y = Dense(1)(dense)
+
+# Setup model
+model = Model(inputs=[user_id_input, movie_id_input], outputs=y)
+model.compile(loss='mse', optimizer='adam')
+
+
+# Fit model
+model.fit([train_user_data, train_movie_data],
+          df_train['rating'],
+          batch_size=256, 
+          epochs=1,
+          validation_split=0.1,
+          shuffle=True)
+
+# Test model
+y_pred = model.predict([test_user_data, test_movie_data])
+y_true = df_test['rating'].values
+
+#  Compute RMSE
+rmse = np.sqrt(mean_squared_error(y_pred=y_pred, y_true=y_true))
+print('\n\nTesting Result With Keras Deep Learning: {:.4f} RMSE'.format(rmse))
+
+model.save('model1')
