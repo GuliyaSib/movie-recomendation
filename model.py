7c55e30bed0beff6a1e6353aed0439b28135ec40
GuliyaSib <sib.guliya@gmail.com>
2023-05-23 19:03:23 +0300

    add  two model

--- model.py
+++ model.py
@@ -85,3 +85,55 @@ rmse = np.sqrt(mean_squared_error(y_pred=y_pred, y_true=y_true))
 print('\n\nTesting Result With Keras Deep Learning: {:.4f} RMSE'.format(rmse))
 
 model.save('model1.h5')
+
+
+# Create user- & movie-id mapping
+
+# Get input variable-sizes
+embedding_size = 20
+
+
+##### Create model
+# Set input layers
+user_id_input = Input(shape=[1], name='user')
+movie_id_input = Input(shape=[1], name='movie')
+
+# Create embedding layers for users and movies
+user_embedding = Embedding(output_dim=embedding_size, 
+                           input_dim=users,
+                           input_length=1, 
+                           name='user_embedding')(user_id_input)
+movie_embedding = Embedding(output_dim=embedding_size, 
+                            input_dim=movies,
+                            input_length=1, 
+                            name='item_embedding')(movie_id_input)
+
+# Reshape the embedding layers
+user_vector = Reshape([embedding_size])(user_embedding)
+movie_vector = Reshape([embedding_size])(movie_embedding)
+
+# Compute dot-product of reshaped embedding layers as prediction
+y = Dot(1, normalize=False)([user_vector, movie_vector])
+
+# Setup model
+model_2 = Model(inputs=[user_id_input, movie_id_input], outputs=y)
+model_2.compile(loss='mse', optimizer='adam')
+
+
+# Fit model
+model_2.fit([train_user_data, train_movie_data],
+          df_train['rating'],
+          batch_size=256, 
+          epochs=1,
+          validation_split=0.1,
+          shuffle=True)
+
+# Test model
+y_pred = model_2.predict([test_user_data, test_movie_data])
+y_true = df_test['rating'].values
+
+#  Compute RMSE
+rmse = np.sqrt(mean_squared_error(y_pred=y_pred, y_true=y_true))
+print('\n\nTesting Result With Keras Matrix-Factorization: {:.4f} RMSE'.format(rmse))
+
+model_2.save('model2.h5')
--- test.py
+++ test.py
@@ -1,18 +1,36 @@
+import time
 import numpy as np
 import pandas as pd
 from keras.models import load_model
 from sklearn.metrics import mean_squared_error
 
-model = load_model('model1.h5')
+model1 = load_model('model1.h5')
+model2 = load_model('model2.h5')
 df = pd.read_csv('test.csv')
 
 test_user_data = df['userId']
 test_movie_data = df['movieId']
 
 # Test model
-y_pred = model.predict([test_user_data, test_movie_data])
+start = time.time()
+y_pred = model1.predict([test_user_data, test_movie_data])
+result_time_1 = time.time() - start
 y_true = df['rating'].values
 
 #  Compute RMSE
-rmse = np.sqrt(mean_squared_error(y_pred=y_pred, y_true=y_true))
-print('\n\nTesting Result With Keras Deep Learning: {:.4f} RMSE'.format(rmse))
+rmse_1 = np.sqrt(mean_squared_error(y_pred=y_pred, y_true=y_true))
+print('\n\nTesting Result With Keras Deep Learning: {:.4f} RMSE'.format(rmse_1))
+
+
+start = time.time()
+y_pred = model2.predict([test_user_data, test_movie_data])
+result_time_2 = time.time() - start
+y_true = df['rating'].values
+
+#  Compute RMSE
+rmse_2 = np.sqrt(mean_squared_error(y_pred=y_pred, y_true=y_true))
+print('\n\nTesting Result With Keras Deep Learning: {:.4f} RMSE'.format(rmse_2))
+
+with open('result.txt', 'w') as f:
+    print('Model_1', rmse_1, result_time_1, file=f)
+    print('Model_2', rmse_2, result_time_2, file=f)
